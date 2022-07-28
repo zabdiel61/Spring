d40392a0cd59bb960f0d9614a7a1959524f772d4
zabdiel1 <jonathanalbayero203@hotmail.com>
2022-07-28 14:13:53 -0600

    primer crud con spring mvc

--- Leccion05/ControlClientes/src/main/java/com/zd/web/ControladorInicio.java
+++ Leccion05/ControlClientes/src/main/java/com/zd/web/ControladorInicio.java
@@ -1,25 +1,51 @@
 package com.zd.web;
 
-import com.zd.dao.PersonaDao;
+import com.zd.domain.Persona;
+import com.zd.servicio.PersonaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Slf4j
 public class ControladorInicio {
 
     @Autowired
-    private PersonaDao personaDao;
+    private PersonaService personaService;
 
     @GetMapping("/")
     public String incio(Model model) {
-        
-        var personas = personaDao.findAll();
-        log.info("ejecutando el controlador Spring MVC");
+
+        var personas = personaService.listarPersonas();
+        log.info("ejecutando el controlador Spring MVC con transacciones");
         model.addAttribute("personas", personas);
         return "index";
+    }
+
+    @GetMapping("/agregar")
+    public String agregar(Persona persona) {
+        return "modificar";
+    } 
+
+    @PostMapping("/guardar")
+    public String guardar(Persona persona) {
+        personaService.guardar(persona);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/editar/{idPersona}")
+    public String editar(Persona persona, Model model){
+         persona = personaService.encontrarPersona(persona);
+         model.addAttribute("persona", persona);
+         return "modificar";
+    }
+    
+    @GetMapping("/eliminar")
+    public String eliminar(Persona persona){
+        personaService.eliminar(persona);
+        return "redirect:/";
     }
 }
